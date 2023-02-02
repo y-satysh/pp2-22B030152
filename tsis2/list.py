d7a2e760e73da7183d78bb529aabfdbad236c3f9
y-satysh <y_satysh@kbtu.kz>
2023-02-03 00:29:39 +0600

    TSIS2

---
+++ tsis2/list.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["abc", 34, True, 40, "male"]
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
